b069c12af3dcddb98aa6acc2eede27cb0c5eb3fa
amitmodi06 <amitmodi06@gmail.com>
2024-01-08 13:21:10 +0000

    FIxed the issue in registering the app. Closes #11

--- my_website/settings.py
+++ my_website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.app.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
