8a83173bd8604abd53e84563393de021e0508809
amitmodi06 <amitmodi06@gmail.com>
2024-01-10 03:05:06 +0000

    Added view for listing all published posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    context = {
+        'posts' : posts
+    }
+    return render(request, 
+                  'blog/post/list.html', 
+                  context)
