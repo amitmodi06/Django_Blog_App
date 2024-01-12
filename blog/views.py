700b8038402fccc7468648ab9803600de7d68db4
amitmodi06 <amitmodi06@gmail.com>
2024-01-12 06:24:19 +0000

    Added exception handling for pagination, to handle if asked page is out of range or not an integer

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
@@ -9,7 +9,15 @@ def post_list(request):
     # pagination with 2 posts per page
     paginator = Paginator(post_list, 2)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # get first page if provided page number is not an integer
+        posts = paginator.page(1)
+    except EmptyPage:
+        # get last page if the provided page number if out of range
+        posts = paginator.page(paginator.num_pages)
 
 
     context = {
